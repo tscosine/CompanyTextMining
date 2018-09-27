36261a254ed3ed04e11758878c545ed8c099faad
tscosine <xiayanbeiyou@126.com>
2018-09-28 00:31:08 +0800

    liyanhong mayun dinglei

--- analysetext.py
+++ analysetext.py
@@ -2,9 +2,10 @@
 import os
 import re
 import xlwt
-
+import codecs
 if __name__ == "__main__":
-    pattern   = re.compile('(.+)(_|-)(\d+).*.txt')
+    # pattern   = re.compile('(.+)(_|-)(\d+).*.txt')
+    pattern   = re.compile('_(\d+)(_|.)')
     text_path = './text'
     row = 0
     workbook = xlwt.Workbook(encoding = 'ascii')
@@ -12,19 +13,23 @@ if __name__ == "__main__":
     textcount = {}
 
     for txt in os.listdir(text_path):
-        # if txt != '马化腾-2015（8）.txt':
+        # if txt != '张朝阳_2015(2).txt':
         #     continue
         print(txt)
         count = 0
-        name  = re.search(pattern,txt).group(1)
-        uname = name.decode('utf-8')
-        year  = re.search(pattern,txt).group(3)
-        file  = open(os.path.join(text_path,txt)).read().decode('utf-8')
+        # name  = re.search(pattern,txt).group(1)
+        uname = u'李彦宏'
+        year  = re.search(pattern,txt).group(1)
+        print(year)
+        file  = codecs.open(os.path.join(text_path,txt),encoding='utf-8').read()
         flag  = False
-        say_word    = u'：'+'|'+u'指出'+'|'+u'表示'+'|'+u'认为'+'|'+u'说'+u'告诉'+u'提到'
+        say_word    = u'：'+'|'+':'+'|'+u'指出'+'|'+u'表示'+'|'+u'认为'+\
+                      '|'+u'说'+'|'+u'告诉'+'|'+u'提到'+'|'+u'坦言'+\
+                      '|'+u'宣布'+'|'+u'看来'+'|'+u'直言'
         push_pattern= re.compile('\s*'+uname+'('+say_word+')')
         pop_pattern = re.compile('^.{0,10}'+u'：')
         for para in file.split('\n'):
+            # print(para)
             if len(para) > 0:
                 if re.match(push_pattern,para):
                     flag = True
@@ -46,9 +51,10 @@ if __name__ == "__main__":
                         sheet.write(row, 2, para)
                         row += 1
                         count += 1
+            # print(flag)
         textcount[txt] = count
     for record in textcount.items():
         if record[1] < 2:
-            print(record[0].decode('utf-8'))
+            print(record[0])
             print(record[1])
     workbook.save('mix.xls')
